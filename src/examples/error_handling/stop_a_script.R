04cc601485f24fef47fb4c19a462f7ee9c26f9cf
Mark Veltzer <mark.veltzer@gmail.com>
2015-02-04 14:24:17 +0200

    started error handling

---
+++ src/examples/error_handling/stop_a_script.R
@@ -0,0 +1,15 @@
+#!/usr/bin/Rscript
+
+# This is a simple example of throwing an exception in R.
+# The exception in this case is NULL. This is not good, you should really throw
+# the reason for why you stop the script.
+#
+# References:
+# help(tryCatch)
+
+for (i in 1:100) {
+	print(i)
+	if (i==3) {
+		stop(NULL)
+	}
+}
--- src/examples/vectors/first_vector.R
+++ src/examples/vectors/first_vector.R
@@ -1,9 +1,23 @@
 #!/usr/bin/Rscript
 
 # This is a first example of using a vector in R.
+#
+# NOTES:
+# - to create a vector using the 'c' combine function.
+# - to access vector elements use myvector[location]
+# - location is 1:n and not 0-[n-1] as most languages use.
+# - myvector[0] does not give you anything but the null string.
+# - accessing vectors in negative locations will return the vector stripped of the
+# value which in the absolute position value of the negative location.
+# - length of the vector is available via 'length(x)'
+# - accessing vector in out-of-range locations will not throw
 
-x=c(6, 7, 8)
+x=c(6, 7, 8, 9)
 print(x[1])
 print(x[2])
 print(x[3])
+print(x[0])
+print(x[-1])
+print(x[-2])
+print(length(x))
 ls.str()
