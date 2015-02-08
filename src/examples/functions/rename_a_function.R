a7787facbd72674e5d14bd4fa3fc9e5cbf587bc7
Mark Veltzer <mark.veltzer@gmail.com>
2015-02-08 19:06:38 +0200

    minor fix

--- src/examples/functions/rename_a_function.R
+++ src/examples/functions/rename_a_function.R
@@ -1,5 +1,9 @@
 #!/usr/bin/Rscript
 
+# This example shows how to overwrite an R
+# builtin function. You can also save the
+# old function pointer on the side for later use.
+
 mean(c(1,2,3,4,5,6))
 
 old_mean <- mean
