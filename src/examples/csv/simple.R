5974b701e5de85c12d419731df4ad31cf0495c16
Mark Veltzer <mark.veltzer@gmail.com>
2015-02-08 18:56:47 +0200

    example of csv

--- src/examples/csv/simple.R
+++ src/examples/csv/simple.R
@@ -2,7 +2,8 @@
 
 # This is a simple example of reading a csv file in R.
 
-heisenberg <- read.csv(file="simple.csv",head=TRUE,sep=",")
-class(heisenberg)
-heisenberg
-summary(heisenberg)
+heisenberg <- read.csv(file="simple.csv", head=TRUE)
+#class(heisenberg)
+#heisenberg
+#summary(heisenberg)
+mean(heisenberg$mass)
