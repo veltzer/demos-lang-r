65fae82ab744748c3155b876cce2ea05549545fc
Mark Veltzer <mark.veltzer@gmail.com>
2015-02-15 14:13:20 +0200

    a few notes

--- src/examples/console/readLines.R
+++ src/examples/console/readLines.R
@@ -1,4 +1,7 @@
 #!/usr/bin/Rscript
 
+# This method of waiting for user input will work
+# in a script (Rscript) well.
+
 cat("Press [enter] to continue...")
 invisible(readLines('stdin', n=1, warn=FALSE))
--- src/examples/console/scan.R
+++ src/examples/console/scan.R
@@ -1,4 +1,7 @@
 #!/usr/bin/Rscript
 
+# This is an example of how to wait for user input
+# This does not work in a script (meaning not in Rscript, only in R)
+
 cat("please press a key...\n")
 invisible(scan(file = "", what = "", nmax = 1))
