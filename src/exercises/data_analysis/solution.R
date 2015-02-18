6ed27046fc6c05fc52fb3860291dfa5f7b9ade81
Mark Veltzer <mark.veltzer@gmail.com>
2015-02-18 19:29:16 +0200

    new exercise for linear regression

--- src/exercises/data_analysis/solution.R
+++ src/exercises/data_analysis/solution.R
@@ -5,9 +5,9 @@
 d <- read.csv(file="smalldata.csv", head=TRUE)
 
 # find out about the data
-#cat('number of rows in d is', nrow(d), '\n')
-#colnames(d)
-#unique(d$district)
+cat('number of rows in d is', nrow(d), '\n')
+colnames(d)
+unique(d$district)
 
 # first question
 # the comma is important
@@ -26,11 +26,17 @@ summary(g)
 # third question
 d19=d[d['district']==19 & d['mathSS']>d['readSS'], ]
 d12=d[d['district']==12 & d['mathSS']>d['readSS'], ]
-d19_sorted=d19[order(d19$mathSS),]
-d12_sorted=d12[order(d12$mathSS),]
-#class(d19)
-#class(d12)
-#class(d19_sorted)
-#class(d12_sorted)
-print(d19[2,])
-print(d12[2,])
+# - for descending
+o19=order(-d19$mathSS)
+o12=order(-d12$mathSS)
+class(o19)
+d19_sorted=d19[o19,]
+d12_sorted=d12[o12,]
+class(d19)
+class(d12)
+class(d19_sorted)
+class(d12_sorted)
+#print(d19_sorted[nrow(d19)-1,]$mathSS)
+#print(d12_sorted[nrow(d12)-1,]$mathSS)
+print(d19_sorted[2,]$mathSS)
+print(d12_sorted[2,]$mathSS)
---
+++ src/exercises/student_linear_regression/README.txt
@@ -0,0 +1,24 @@
+Use the student data as basic for analysis.
+
+Pick 2 years: Y1, and Y2.
+
+Use Y1 as data and Y2 for prediction.
+
+Predict Y2 mathSS and readSS from Y1. Use any columns you like.
+
+Estimate how accurate your data was.
+
+Find out which dimensions of the data you can drop without loosing
+much accuracy.
+
+Divide the population of two years to students that will be used for building
+the model and students that will be used for checking the model (by percentage
+or any other means).
+
+No need for anything more complicated than a linear model.
+
+Use:
+- Chi-Squared for testing which columns are significant.
+- lm() for linear modeling.
+
+Write everything as a script.
