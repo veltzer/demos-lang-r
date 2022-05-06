c62845e765b0a147e488b85e5220a823e3e73e84
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-06 03:33:50 +0300

    removing packages

--- config/deps.py
+++ config/deps.py
@@ -6,9 +6,9 @@ packages=[
     "r-doc-html",
     "r-doc-pdf",
     "build-essential",
-    # R packages
-    "r-api-3.5",
-    "r-cran-sm",
-    "r-cran-plotrix",
-    "r-cran-snow",
+    # R packages (have problems installing in CI)
+    # "r-api-3.5",
+    # "r-cran-sm",
+    # "r-cran-plotrix",
+    # "r-cran-snow",
 ]
