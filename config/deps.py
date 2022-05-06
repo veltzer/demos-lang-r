d6932e2535d155355baa99bbf2bcc01498d6406c
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-06 03:07:34 +0300

    removed templardefs

--- config/deps.py
+++ config/deps.py
@@ -1,4 +1,13 @@
 packages=[
-    'r-base',
-    'build-essential',
+    # R core
+    "r-base",
+    "r-base-core",
+    "r-doc-info",
+    "r-doc-html",
+    "r-doc-pdf",
+    "build-essential",
+    # R packages
+    "r-cran-sm",
+    "r-cran-plotrix",
+    "r-cran-snow",
 ]
