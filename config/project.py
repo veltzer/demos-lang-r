2b06963643c651d1ff4c41f1da9f9c5d1b1ff027
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-18 13:50:58 +0300

    new config

--- config/project.py
+++ config/project.py
@@ -1,4 +1,1 @@
-project_name = "demos-r"
-project_long_description = "Demos for the r language"
-project_year_started = 2014
-project_description = """Demos for the r language"""
+description_short = "Demos for the r language"
